6dcf159d5aa91d8c9405dc5412eb2b8c21c203f0
fernanda.penuelas <penuelasfer@gmail.com>
2023-05-22 11:20:16 -0600

    cambio de nombre a la función

--- preparacion_datos/funciones_cruces_limpieza.py
+++ preparacion_datos/funciones_cruces_limpieza.py
@@ -8,7 +8,7 @@ def choose_city(gdf_zonas):
     gdf_mun = gdf_zonas[gdf_zonas.CVE_MUN == zona]
     #gdf_mun.plot()
     return gdf_mun
-def cruce_zonas_rurales_urbanas(gdf_zonas,gdf_construcciones):
+def cruce_limpieza_shapes(gdf_zonas,gdf_construcciones):
     gdf_mun = choose_city(gdf_zonas)
     gdf_mun = gdf_mun.to_crs(3857)
     gdf_cruce= gdf_construcciones.sjoin(gdf_mun,how='left')
